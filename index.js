0b2f11159dd0311c698f95dece9d6c0161579626
salonishalu2000@gmail.com <salonishalu2000@gmail.com>
2023-04-28 12:49:27 +0530

    done

---
+++ index.js
@@ -0,0 +1,13 @@
+const str = "hello"
+
+console.log(str)
+
+const str2 = "world"
+
+console.log(str + " " + str2)
+
+if (str.length > 4) {
+    console.log("trueeeeeee")
+} else {
+    console.log("false.....")
+}
